3564f7cb2d0fead4d4fd81f483870032bbc7471e
TYW <167500749+tanyuanwen@users.noreply.github.com>
2025-04-10 20:28:03 +0800

    Add files via upload

---
+++ src/main/java/cn/tywmc/sh1/ShanghaiMetroLineOneModClient.java
@@ -0,0 +1,10 @@
+package cn.tywmc.sh1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShanghaiMetroLineOneModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
